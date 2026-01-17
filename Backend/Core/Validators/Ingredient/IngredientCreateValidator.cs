c023d9cb95143677cef5e262e66d26720d66bd19
P-Nastia <nastia.pyvovarchuck@gmail.com>
2026-01-17 14:55:54 +0200

    is good add ingredient validators

---
+++ Backend/Core/Validators/Ingredient/IngredientCreateValidator.cs
@@ -0,0 +1,34 @@
+﻿
+
+using Core.Model.Recipe.Ingredient;
+using Domain.Data;
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Validators.Ingredient;
+
+public class IngredientCreateValidator : AbstractValidator<IngredientCreateModel>
+{
+    public IngredientCreateValidator(AppDbContext context)
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Назва обов'язкова")
+            .Must(name => !String.IsNullOrEmpty(name))
+            .WithMessage("Назва не може бути null або empty")
+            .DependentRules(() =>
+            {
+                RuleFor(x => x.Name)
+                    .MustAsync(async (name, cancellation) =>
+                    !await context.Ingredients.AnyAsync(c => c.Name.ToLower() == name.ToLower().Trim(), cancellation))
+                .WithMessage("Інгредієнт з такою назвою вже існує");
+            })
+            .MaximumLength(250)
+            .WithMessage("Назва має бути не довшою, ніж 250 символів");
+
+        RuleFor(x => x.ImageFile)
+            .NotEmpty()
+            .WithMessage("Image file обов'язковий");
+    }
+}
