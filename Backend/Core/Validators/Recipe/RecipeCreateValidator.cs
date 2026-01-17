71395fc7042b87b780a4cd28f94ffbf67fff3f04
P-Nastia <nastia.pyvovarchuck@gmail.com>
2026-01-17 16:35:40 +0200

    add create recipe validator

---
+++ Backend/Core/Validators/Recipe/RecipeCreateValidator.cs
@@ -0,0 +1,83 @@
+﻿
+using Core.Model.Recipe;
+using Domain.Data;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+
+namespace Core.Validators.Recipe;
+
+public class RecipeCreateValidator : AbstractValidator<RecipeCreateModel>
+{
+    public RecipeCreateValidator(AppDbContext context)
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Назва обов'язкова")
+            .Must(name => !String.IsNullOrEmpty(name))
+            .WithMessage("Назва не може бути null або empty")
+            .DependentRules(() =>
+            {
+                RuleFor(x => x.Name)
+                    .MustAsync(async (name, cancellation) =>
+                    !await context.Ingredients.AnyAsync(c => c.Name.ToLower() == name.ToLower().Trim(), cancellation))
+                .WithMessage("Рецепт з такою назвою вже існує");
+            })
+            .MaximumLength(300)
+            .WithMessage("Назва має бути не довшою, ніж 300 символів");
+
+        RuleFor(x => x.Image)
+            .NotEmpty()
+            .WithMessage("Image file обов'язковий");
+
+        RuleFor(x => x.Slug)
+            .NotEmpty()
+            .WithMessage("Слаг обов'язковий")
+            .Must(slug => !string.IsNullOrEmpty(slug))
+            .WithMessage("Слаг не може бути empty або null")
+            .DependentRules(() =>
+            {
+                RuleFor(x => x.Slug)
+                    .MustAsync(async (slug, cancellation) =>
+                    !await context.Recipes.AnyAsync(c => c.Slug.ToLower() == slug.ToLower().Trim(), cancellation))
+                .WithMessage("Категорія з таким слагом вже існує");
+            })
+            .MaximumLength(350)
+            .WithMessage("Слаг має бути не довшим, ніж 250 символів");
+
+        RuleFor(x => x.Instruction)
+            .NotEmpty()
+            .WithMessage("Кроки приготування обов'язкові")
+            .MinimumLength(50)
+            .WithMessage("Інструкція не може бути коротшою за 50 символів");
+
+        RuleFor(x => x.CategoryId)
+            .NotEmpty()
+            .WithMessage("Оберіть обов'язково категорію")
+            .Must(cat => cat > 0)
+            .WithMessage("Такої категорії не існує");
+
+        RuleFor(x => x.IngredientsJson)
+           .Cascade(CascadeMode.Stop)
+           .NotEmpty()
+           .WithMessage("Інгредієнти обов'язкові")
+           .MustAsync(async (json, ct) =>
+               await ValidateIngredientsAsync(json!, context, ct))
+           .WithMessage("Всі або окремий інгредієнт заповнений неправильно");
+    }
+
+    private async Task<bool> ValidateIngredientsAsync(string json, AppDbContext context, CancellationToken ct)
+    {
+        var ingredients = JsonSerializer.Deserialize<List<RecipeIngredientCreateModel>>(json);
+        if (ingredients == null)
+            return false;
+        var validator = new RecipeIngredientValidator(context);
+        foreach(var ingr in ingredients)
+        {
+            var res = await validator.ValidateAsync(ingr, ct);
+            if (!res.IsValid)
+                return false;
+        }
+        return true;
+    }
+}
---
+++ Backend/Core/Validators/Recipe/RecipeIngredientValidator.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Core.Model.Recipe;
+using Domain.Data;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Validators.Recipe;
+
+public class RecipeIngredientValidator : AbstractValidator<RecipeIngredientCreateModel>
+{
+    public RecipeIngredientValidator(AppDbContext context)
+    {
+        RuleFor(x => x.IngredientId)
+            .GreaterThan(0)
+            .MustAsync(async (id, ct) => await context.Ingredients.AnyAsync(i => i.Id == id, ct))
+            .WithMessage("Інгредієнт не існує");
+
+        RuleFor(x => x.IngredientUnitId)
+            .GreaterThan(0)
+            .MustAsync(async (id, ct) => await context.IngredientUnits.AnyAsync(i => i.Id == id, ct))
+            .WithMessage("Такої мірки не існує");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+            .WithMessage("Кількість інгредієнт має бути більшою 0");
+    }
+}
